04c3f7f66ce8e03fc18138f1d543f937ea2c6976
Ovi Sofariu <ovi@sofariu.com>
2020-01-28 10:53:53 -0500

    final clean-up before merge

--- notebooks/Cmh_Parking_Violation.py
+++ notebooks/Cmh_Parking_Violation.py
@@ -17,27 +17,40 @@
 
 # ## Imports
 #
-# * pandas
-# * string
-#     * parse string amounts to translate into floats
+# * pandas, numpy
+#     * manipulate all the things
+# * matplotlib, seaborn
+#     * histograms, pairplots, etc.
+# * osmnx
+#     * display violation location on a map
+# * networkx
+#     * calculate shortest path
+#     * set-up graph for mapping a graph with heatmap attributes
+# * folium
+#     * example of interactive maps
 
 # Edit notebook in external editor, so don't auto-save from jupyter notebook
+# this is a trial.. looking for a good way to preserve diffs
 # %autosave 0
 
 # +
 # #%matplotlib inline
 
-import numpy as np
 import datetime
+import hashlib
+import math
+
+
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
 import osmnx as ox
 import networkx as nx
+
 import folium
 from IPython.display import IFrame
-# to hash 'string' badge numbers to numeric so that we can encode them as colors
-import hashlib
 
 # -
 
@@ -84,7 +97,7 @@ meters = pd.read_csv("../data/raw/d9b11b8f-67f3-48c4-8831-0f22d93166ce")
 
 # ## Extract interesting data from Violations dataset
 
-# work with the most promising columns:
+# work with the most promising (on first pass) columns:
 violations_columns_use = ['ticket', 'entity', 'meter', 'iss dt', 'due', 'fine', 'make', 'iss time',
                           'badge', 'pay amt', 'lat', 'long']
 
@@ -111,6 +124,7 @@ print(violations[['iss dt', 'iss time', 'issue_date', 'year', 'dayOfWeek', 'hour
 
 # #### Show distribution of fine and pay_amt
 
+# +
 f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 5))
 sns.distplot(violations[violations['amt_float'] < 100]['amt_float'], bins=20, ax=ax1)
 sns.distplot(violations[(violations['amt_float'] > 100) & (violations['amt_float'] <= 200)]['amt_float'], bins=20,
@@ -118,8 +132,18 @@ sns.distplot(violations[(violations['amt_float'] > 100) & (violations['amt_float
 sns.distplot(violations[(violations['amt_float'] > 200) & (violations['amt_float'] <= 600)]['amt_float'], bins=20,
              ax=ax3)
 
+# TODO - show the numer of samples on each graph.. maybe also add another graph that shows all the data
+# currently you can't tell what the overall distribution of fine amounts is.
+# -
+
+# this is an outlyer..
+# TODO - pay amount currently strips out the negative, but the example below amy be a credit.  We might
+# want to parse the amount so that negative amouts show up.. or alternately filter them out.
 larger_amt = violations[(violations['amt_float'] >= 600)]
 print(larger_amt)
+
+_, ax0 = plt.subplots(1, 1, figsize=(20, 5))
+sns.distplot(violations['fine_float'], bins=100, ax=ax0)
 
 # +
 _, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20, 5))
@@ -128,6 +152,7 @@ sns.distplot(violations[violations['fine_float'] < 100]['fine_float'], bins=20, 
 sns.distplot(violations[(violations['fine_float'] >= 100) & (violations['fine_float'] < 200)]['fine_float'], bins=20,
              ax=ax2)
 sns.distplot(violations[(violations['fine_float'] >= 200) & (violations['fine_float'] < 600)]['fine_float'], ax=ax3)
+# # TODO - show the numer of samples on each graph (same as for the pay amount graphs)
 
 # +
 violations.info()
@@ -136,16 +161,13 @@ violations.info()
 violations.sample(10)
 # -
 
-# distribution of outliers - entities who got more than 25 tickets
+# aggregate pay amount and fine by entity (who received the fine)
 entity_tickets = violations[['entity', 'amt_float', 'fine_float']].groupby('entity').agg(['count', 'sum']).sort_values(
     [('amt_float', 'count')], ascending=False)
+# distribution of outliers - entities who got more than 25 tickets
 print(entity_tickets.head(25))
 
-# #### distribution of fines
-
-_, ax1 = plt.subplots(1, 1, figsize=(20, 5))
-sns.distplot(entity_tickets[('fine_float', 'count')]
-             [(entity_tickets[('fine_float', 'count')] >= 25)], bins=50, ax=ax1)
+# #### distribution of violations over time
 
 # wonder why number of violations issued went down the last three years?
 violations['year'].value_counts()
@@ -171,10 +193,15 @@ agent_business = violations[['badge', 'year', 'entity', 'ticket', 'fine_float']]
 
 agent_business.columns = ['fine_mean', 'fine_min', 'fine_max', 'entity_count']
 agent_business.reset_index().sort_values('entity_count', ascending=False).head(25)
-# -
 
+# +
 violations_clean = violations[['iss time', 'fine_float', 'dayOfWeek', 'hour', 'year']].dropna()
-# sns.pairplot(violations_clean)
+
+# I don't think this tells much of a story, but keeping the pairplot as an example.. maybe
+# someone will see something in this data.
+
+sns.pairplot(violations_clean)
+# -
 
 # #### How many tickets are generally given each day?
 
@@ -191,7 +218,8 @@ except IOError:
 fig, ax = ox.plot_graph(G)
 
 # +
-# get min, max for lat long as coordinates from G, to get bounds
+# Get min, max for lat long as coordinates from G, to get bounds for columbus
+# We will use these bounds to clean up the lat/long in the violations data, which is not very clean
 cmh_gdf = ox.graph_to_gdfs(G)
 print(cmh_gdf[0].x.agg(['min', 'max']))
 print(cmh_gdf[0].y.agg(['min', 'max']))
@@ -218,6 +246,9 @@ meters.info()
 # +
 # meters[meters['meter number'] == 'EN529']
 
+# We're joining the meters and violations data frames on column 'Meter'.
+# so we will rename this column to be able to do the merge
+# TODO: figure out why I can't rename this to 'meter' - it forces capitalization on this data frame
 meters = meters.rename(columns={'Meter Number': 'Meter'})
 meters.info()
 # -
@@ -234,7 +265,7 @@ violations_x_meters = violations.merge(meters, how='left', on='Meter')
 
 # #### Set the violation location from the meters dataset if available
 
-# TODO: make faster if possible.. vectorization?
+# TODO: make faster; Dan suggested using a WHERE cause which takes advantage of vectorization
 fudge = 0.2
 violations_x_meters['lat_combined'] = violations_x_meters \
     .apply(lambda p: p['Lat']
@@ -259,12 +290,15 @@ violations_by_day = violations[['issue_date', 'Meter']].groupby(['issue_date']).
 sns.distplot(violations_by_day)
 print(violations_by_day)
 
+# This if the final data that we will be plotting on a map
 violations_x_meters.head(12)
 
 _, ax_violations = ox.plot_graph(G, fig_height=15, show=False, close=False)
 ax_violations.scatter(violations_x_meters['long_combined'], violations_x_meters['lat_combined'], alpha=0.5, c='red')
 
 # +
+# Pick a random data set that's somewhat clustered, to create a heat map.
+
 heat_tickets = violations_x_meters[(violations_x_meters['badge'] == '19') &
                                    (violations_x_meters['issue_date'] == '2014-02-13')] \
     .sort_values(by='iss time') \
@@ -273,7 +307,9 @@ heat_tickets = violations_x_meters[(violations_x_meters['badge'] == '19') &
 print(heat_tickets.shape)
 
 # +
-# Would like to create a heat map for just this area
+# Would like to create a heat map for just this area, so that we can zoom into it on the map
+# initially I plotted the heat map for these 63 points on the entire map of columbus..
+# and the highlighted area was too small.
 bbox = ox.core.bbox_from_point((39.966358, -83.001397), 2500)
 north, south, east, west = bbox
 
@@ -283,15 +319,21 @@ ax_small_violations.scatter(heat_tickets['long_combined'], heat_tickets['lat_com
 
 
 # +
-# get nearest node to each meter location
+# get nearest node (on the OSMNX graph) to each meter location; we can't just add these points
+# to the Open Street map without connecting them to the existing graph; perhaps we can do something
+# more clever there to get better accuracy on these locations.  We did set "simplify=false" when
+# importing the original map of Columbus to hopefully get more points near meters.
 
 def heat_nodes(heat_tickets):
     for ticket in heat_tickets.values:
-        meter, lat, long = ticket
+        _, lat, long = ticket
         yield ox.get_nearest_node(G_small, (lat, long))
 
 
 # +
+# temporarily bringing in heatmapx and making it work with OpenStreet maps until we can make
+# the package support our use case
+
 # # %load ~/Library/Caches/pypoetry/virtualenvs/cmh-packing-violations-JzEtr0-_-py3.7/lib/python3.7/site-packages/heatmapx/__init__.py
 __version__ = '0.1.0'
 __all__ = ['temperature_graph']
@@ -408,21 +450,24 @@ def _update_node_temperatures(G, nodes, heat_key, heat_increment, visited_nodes)
 
 # -
 
+# calculate the heat of each node; this adds a 'heat' attribute to each node in the graph
+# the more near connections to other 'hot' nodes the higher the heat value
 T = temperature_graph(G_small, heat_nodes(heat_tickets), heat_increments=1, depth_limit=10)
 
-# +
+# # copy node attributes from G_small
+nx.set_node_attributes(T, {node: data for node, data in G_small.nodes(data=True)})
 
-nx.set_node_attributes(T, {node: data for node, data in G.nodes(data=True)})
-# -
-
+# # copy edge attributes from G_small
 nx.set_edge_attributes(
     T, {(source, target, key): data
         for source, target, key, data
         in G_small.edges(keys=True, data=True)})
 
+# OSMNX wants these set in order to be able to graph T
 T.graph['crs'] = G_small.graph['crs']
 T.graph['name'] = G_small.graph['name']
 
+# extract node and edge heat values, so we can map each value to a different color on the grapgh
 heat_node_values = [heat for (id, heat) in list(T.nodes(data='heat'))]
 heat_edge_values = [heat for (source, dest, heat) in list(T.edges(data='heat'))]
 print(len(heat_node_values), len(heat_edge_values))
@@ -435,9 +480,10 @@ map_heat_to_color = {
     15: "#ef5000", 16: "#dc4900", 17: "#c84300", 18: "#b43c00", 19: "#a13600",
     20: "#8d2f00", 21: "#7a2900", 22: "#662200", 23: "#662200", 24: "#662200"}
 
-import math
-
-
+# TODO: test this function!  Added pytest to the project, but need to extract code that we need
+# to test into separate files so we can test them.
+# basically when we have more heat values than we have colors in our map (we have 25 colors), then
+# we pick an existing color by applying a ratio..
 def heat_to_color(index, max_index):
     use_index = index
     if max_index >= len(map_heat_to_color):
@@ -456,6 +502,8 @@ print(len(node_heat_colors), len(edge_heat_colors))
 ox.plot_graph(T, fig_height=15, show=False, close=False, node_color=node_heat_colors, edge_color=edge_heat_colors)
 
 # ### Plot a route from two points on the graph
+#
+# This section is just used as a demonstration for now.
 
 tickets = heat_tickets
 origin_point = (tickets[0:1].lat_combined.iloc[0], tickets[0:1].long_combined.iloc[0])
@@ -466,20 +514,23 @@ destination_node = ox.get_nearest_node(G, destination_point)
 print(origin_node, destination_node)
 
 # +
-# The plan is to plot a route between every visited parking meter, ordered by time to see the most likely
-# route taken.  To start with we are plotting a route between only the first and last visited points, to
-# get a hang of how to plot routes.
+# TODO: make this useful; for example, plot a route between every visited parking meter, ordered by time 
+#to see the most likely route taken.  
+
+#To start with we are plotting a route between only the first and last visited points, to get a hang 
+# of how to plot routes.
 
 bbox = ox.core.bbox_from_point((39.966358, -83.001397), 2500)
 north, south, east, west = bbox
 
-G_small2 = ox.graph_from_bbox(north, south, east, west, network_type='drive', simplify=False, retain_all=True)
+G_small_for_route = ox.graph_from_bbox(north, south, east, west, 
+                                       network_type='drive', simplify=False, retain_all=True)
 
-route = nx.shortest_path(G_small2, origin_node, destination_node, weight='length')
+route = nx.shortest_path(G_small_for_route, origin_node, destination_node, weight='length')
 
 # when plotting a route from a bounded box it fails.. something gets extracted, but it works with full map
 # projected = ox.project_graph(G_small)
-ox.plot_graph_route(G_small2, route, origin_point=origin_point,
+ox.plot_graph_route(G_small_for_route, route, origin_point=origin_point,
                     destination_point=destination_point, fig_height=15, show=False, close=False)
 
 # -
@@ -487,7 +538,9 @@ ox.plot_graph_route(G_small2, route, origin_point=origin_point,
 # ### Color-code tickets by issuing Agent
 
 # +
-# 2013-01-03
+# Plot violations given on a single day, 2013-01-03, color-coded by the individual issuing agent
+# TODO: This could be expanded to compare different agents by day or time
+
 violations_for_day = violations_x_meters[violations_x_meters['issue_date'] == '2013-01-03']
 
 bbox = ox.core.bbox_from_point((39.977110, -83.003500), 4000)
@@ -495,12 +548,14 @@ north, south, east, west = bbox
 G0 = ox.graph_from_bbox(north, south, east, west, network_type='drive_service')
 _, ax_day_violations = ox.plot_graph(G0, fig_height=15, show=False, close=False)
 
-
+# badge number is a string; this function maps that to an integer value
+# TODO: test this function!
 def encode_badge(badge):
     encoded_badge = str(badge).encode('utf-8')
     return int(hashlib.sha1(encoded_badge).hexdigest(), 16) % (10 ** 8) % 10000
 
-
+# list of colors for each violation, where an individual badge gets its own color
+# we pass this list to the `scatter` function to it can pick the right color for each point
 badge_to_colors = violations_for_day['badge'].map(encode_badge)
 ax_day_violations.scatter(violations_for_day['long_combined'], violations_for_day['lat_combined'],
                           alpha=0.5, c=badge_to_colors, marker='h')
@@ -533,8 +588,14 @@ night_violations = short_north_violations[(short_north_violations['iss time'] >=
                                           (short_north_violations['iss time'] < 500)]
 
 # #### Create bounding box 1 mile around the Forge, and plot violations around this area
+#
+# The point here is to see how violations vary by time for the day, for the same area.
 
 # +
+# TODO: The colors for each `scatter` graph overlap on top of each other, so it's hard to see
+# which area favors violations at a particular time.  It's been suggested that we stack these
+# points as vertical projections, showing them on a third coordinate.
+
 _, ax_time_of_day = ox.plot_graph(G1, fig_height=10, show=False, close=False)
 # ax.scatter(violations_x_meters['long_combined'], violations_x_meters['lat_combined'], alpha=0.2, c='red')
 
@@ -553,7 +614,7 @@ plt.tight_layout()
 
 # -
 
-# ### Hot meters -- meters that got most expensive tickets
+# ### Hot meters -- meters that raised the highest revenue
 
 grp = violations_x_meters[['Meter', 'amt_float']].dropna() \
     .groupby(['Meter'], as_index=False) \
@@ -564,9 +625,18 @@ sns.heatmap(grp)
 
 hot_meter_numbers = pd.Series(grp.index)
 hot_meters = short_north_violations[violations_x_meters['Meter'].isin(hot_meter_numbers)]
+# TODO: fix warning
 
 # +
-# plot the route with folium
+# Mark the hottest meters (as defined above) around the Forge. We add them as markers on
+# a folium map.
+
+# Note: this is done in Javascript, and it's slow.  You don't want to do this with thousands or more points
+
+# TODO: you can set each icon dynamically, but I had trouble choosing a different one; I trie both 
+# font-awesome and glyphicon icons, and it wouldn't pick the icon I wanted.  Figure out how to change that.
+
+# TODO: Find something more interesting to plot.. like meters that got least tickets?
 route_map = ox.plot_graph_folium(G1, popup_attribute='name', edge_width=2)
 
 folium.Marker(
